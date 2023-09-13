d4eb45c1a1b41f96c51171fb8b618c1afd90a9ef
ChibsDev <chibsdev@gmail.com>
2023-09-13 08:24:44 +0100

    welcome message made with animations... Think I am going to take time and consider how i want to display the projects.

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ window.addEventListener('scroll', () => {
         header.classList.remove('sticky');
     }
 });
+
+
+function animateHeroMessages() {
+    var heroMessages = document.querySelectorAll('.hero-message h1');
+    var subMessage = document.querySelector('#subMessage');
+  
+    heroMessages[0].classList.add('fade-in');
+  
+    setTimeout(function() {
+      heroMessages[0].classList.remove('fade-in');
+      heroMessages[0].classList.add('fade-out');
+    }, 1500); 
+    setTimeout(function() {
+      heroMessages[1].classList.add('fade-in');
+    }, 3500); 
+    setTimeout(function() {
+      heroMessages[1].classList.remove('fade-in');
+      heroMessages[1].classList.add('fade-out');
+    }, 5500); 
+  
+    setTimeout(function() {
+      heroMessages[2].classList.add('fade-in');
+      subMessage.classList.add('drop-in');
+    }, 7000); 
+  }
+  
+  window.onload = animateHeroMessages;
